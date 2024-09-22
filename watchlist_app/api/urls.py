ccf4055b6b12bf01159019340bcbedc82bf37f45
moozes <m.khodja@esi-sba.dz>
2024-09-22 10:09:37 +0100

    phase1: Serialzer with class based view APIView

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from watchlist_app.api.views import movie_detail, movie_list
+from watchlist_app.api.views import MovieListAV, MovieDetailAV
 urlpatterns = [
-    path('list/' , movie_list),
-    path('<int:pk>' , movie_detail),
+    path('list/' , MovieListAV.as_view()),
+    path('<int:pk>' , MovieDetailAV.as_view()),
 ]
