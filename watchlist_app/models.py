f3285c781307a8576090d52023abff340b811bcf
moozes <m.khodja@esi-sba.dz>
2024-09-25 21:52:36 +0100

    Add reviews model and serializer

--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class StreamPlatform(models.Model):
     name = models.CharField(max_length=200)
@@ -16,3 +17,15 @@ class WatchList(models.Model):
     platform = models.ForeignKey(StreamPlatform, on_delete=models.CASCADE, related_name="watchlist")
     def __str__(self) -> str:
         return self.title
+
+
+class Review(models.Model):
+    rating = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    description = models.CharField(max_length=200)
+    watchlist = models.ForeignKey(WatchList, on_delete=models.CASCADE, related_name='reviews')
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"{self.watchlist.title} (rating: {self.rating})"
