c0de16a8ff6fe847ed139fcc148cc9f8a2e72568
moozes <m.khodja@esi-sba.dz>
2024-10-02 08:01:30 +0100

    Add a trailing slash to all the endpoints

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -14,11 +14,11 @@ router.register('stream', StreamPlatformAV, basename='stream')
 
 urlpatterns = [
     path('watch/' , WatchListAV.as_view()),
-    path('watch/<int:pk>' , WatchDetailAV.as_view()),
+    path('watch/<int:pk>/' , WatchDetailAV.as_view()),
 
-    path('watch/<int:pk>/reviews/create' , ReviewCreate.as_view()),
+    path('watch/<int:pk>/reviews/create/' , ReviewCreate.as_view()),
     path('watch/<int:pk>/reviews/' , Reviews.as_view()),
-    path('watch/reviews/<int:pk>' , ReviewDetail.as_view()),
+    path('watch/reviews/<int:pk>/' , ReviewDetail.as_view()),
 
     path('', include(router.urls))
 ]
