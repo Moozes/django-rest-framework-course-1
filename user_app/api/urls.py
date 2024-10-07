50f0190da7c08f6e28a8072b7c0916d206ddb992
moozes <m.khodja@esi-sba.dz>
2024-10-07 20:58:26 +0100

    Add logout view

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
-from user_app.api.views import registration_view
+from user_app.api.views import registration_view, logout_view
 
 urlpatterns = [
     path('login/', obtain_auth_token),
-    path('register/', registration_view)
+    path('register/', registration_view),
+    path('logout/', logout_view)
 ]
--- user_app/api/views.py
+++ user_app/api/views.py
@@ -1,7 +1,19 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authtoken.models import Token
 from user_app.api.serializers import RegistrationSerializer
+
+# models need to be imported some where for the post_save signal to be executed
+from user_app import models
+
+
+@api_view(['POST'])
+def logout_view(request):
+
+    if request.method == 'POST':
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
 
 @api_view(["POST"])
 def registration_view(request):
@@ -9,7 +21,16 @@ def registration_view(request):
     if request.method == "POST":
         serializer = RegistrationSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
+            # the token will be generated after this save function call
+            user = serializer.save()
+
+            # create a custom response object to include token
+            data = {}
+            data['username'] = user.username
+            data['email'] = user.email
+
+            data['token'] = Token.objects.get(user=user).key
+
+            return Response(data)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
